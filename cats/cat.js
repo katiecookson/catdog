09840aebc108c251a885631e63eba5f2ccb60e20
Katie Cookson <katie.cookson@bjss.com>
2018-10-30 14:40:29 +0000

    CAT-4 creating mouth

--- cats/cat.js
+++ cats/cat.js
@@ -2,4 +2,5 @@ export class Cat {
     setBody(value) { this.body = value; }
     setNumberOfLegs(value) { this.numberOfLegs = value; }
     setTail(value) { this.tail = value; }
+    setMouth(value) { this.mouth = value; }
 }
